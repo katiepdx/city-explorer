ff2a078c64a0c9af6fdfad75daf1f3723a0190c4
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-08-11 10:39:36 -0700

    deploy

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+require('dotenv').config();
 const cors = require('cors');
 const { geoData } = require('./data/geo.js');
 const weatherData = require('./data/weather.js');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // End point
 app.get('/', (req, res) => {
